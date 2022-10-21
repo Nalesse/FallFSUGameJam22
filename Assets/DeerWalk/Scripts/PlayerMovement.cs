39fd7686a40eb904dc4cfbb885caf9ba00c5dc0c
delton <hulbert.delton@gmail.com>
2022-10-21 08:38:54 -0600

    refactoring sound, adding assets and updating prefabs

--- Assets/DeerWalk/Scripts/PlayerMovement.cs
+++ Assets/DeerWalk/Scripts/PlayerMovement.cs
@@ -16,24 +16,6 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 currentInputVector;
     private Vector2 smoothInputVelocity;
 
-    private AudioSource _audioSource;
-    private AudioSource audioSource
-    {
-        get
-        {
-            if (_audioSource == null)
-            {
-                _audioSource = GetComponent<AudioSource>();
-                if (_audioSource == null)
-                {
-                    Debug.LogError($"AudioSource not found on {name}");
-                    return null;
-                }
-            }
-            return _audioSource;
-        }
-    }
-
     private Animator _animator;
     private Animator animator
     {
@@ -57,7 +39,6 @@ public class PlayerMovement : MonoBehaviour
     //Inspector vars
     [SerializeField] private float movementSpeed;
     [SerializeField] private float smoothInputSpeed = .2f;
-    [SerializeField] private List<AudioClip> footstepSounds;
 
     private void Awake()
     {
@@ -104,17 +85,5 @@ public class PlayerMovement : MonoBehaviour
 
         animator.SetFloat("MoveX", Mathf.Clamp01(currentInputVector.x) * (isRunning ? 2f : 1f));
         animator.SetFloat("MoveY", Mathf.Clamp01(currentInputVector.y) * (isRunning ? 2f : 1f));
-
-    }
-
-    public void DoFootstep()
-    {
-        if (footstepSounds.Count == 0)
-            return;
-
-        int i = UnityEngine.Random.Range(0, footstepSounds.Count);
-
-        audioSource.clip = footstepSounds[i];
-        audioSource.Play();
     }
 }
